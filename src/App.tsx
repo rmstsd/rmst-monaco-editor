74653ceaa322e208408bf23fbe8f89f08aeb5075
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-06-25 19:53:37 +0800

    decorator

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
 import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import { jsCode } from './code'
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
@@ -28,6 +29,17 @@ self.MonacoEnvironment = {
 
 function App() {
   const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor>()
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>()
+  editorRef.current = editor
+
+  const getAllBreakDots = () => {
+    const model = editorRef.current.getModel()
+    const des = model.getAllDecorations()
+
+    const breakDots = des.filter(item => item.options.glyphMarginClassName?.includes('myGlyphMarginClass'))
+
+    return breakDots
+  }
 
   useEffect(() => {
     console.log('MouseTargetType', monaco.editor.MouseTargetType)
@@ -40,54 +52,13 @@ function App() {
       glyphMargin: true
     })
 
+    editor.onDidChangeCursorPosition(evt => {
+      console.log(evt)
+    })
+
     setEditor(editor)
 
-    const decorationMap: Record<
-      number,
-      {
-        config: monaco.editor.IModelDeltaDecoration
-        instance: monaco.editor.IEditorDecorationsCollection
-      }
-    > = {}
-
     let currLineNumber = null
-
-    function mouseEnterLine(lineNumber: number) {
-      console.log('移入了', lineNumber)
-
-      if (decorationMap[lineNumber]) {
-        return
-      }
-
-      const config = {
-        range: new monaco.Range(lineNumber, 1, lineNumber, 1),
-        options: { glyphMarginClassName: 'myGlyphMarginClass' }
-      }
-
-      decorationMap[lineNumber] = {
-        config: config,
-        instance: editor.createDecorationsCollection([config])
-      }
-    }
-
-    function mouseLeaveLine(lineNumber: number) {
-      console.log('移出了', currLineNumber)
-
-      if (!decorationMap[lineNumber]) {
-        return
-      }
-
-      if (
-        decorationMap[lineNumber].config.options.glyphMarginClassName.includes('myGlyphMarginClass-active')
-      ) {
-        return
-      }
-
-      const curLineDecoration = decorationMap[lineNumber].instance
-
-      curLineDecoration.clear()
-      Reflect.deleteProperty(decorationMap, lineNumber)
-    }
 
     editor.onMouseMove(evt => {
       // console.log('mousemove - ', evt)
@@ -128,43 +99,78 @@ function App() {
       if (evt.target.type === monaco.editor.MouseTargetType.GUTTER_GLYPH_MARGIN) {
         const { position } = evt.target
 
-        const newGlyphMarginClassName = decorationMap[
-          position.lineNumber
-        ].config.options.glyphMarginClassName.includes('myGlyphMarginClass-active')
+        const dots = getAllBreakDots()
+
+        const currLineDot = dots.find(item => item.range.startLineNumber === position.lineNumber)
+
+        const glyphMarginClassName = currLineDot.options.glyphMarginClassName.includes(
+          'myGlyphMarginClass-active'
+        )
           ? 'myGlyphMarginClass'
           : 'myGlyphMarginClass myGlyphMarginClass-active'
 
-        decorationMap[position.lineNumber].config.options.glyphMarginClassName = newGlyphMarginClassName
+        const newDecorations = [
+          {
+            range: new monaco.Range(position.lineNumber, 1, position.lineNumber, 1),
+            options: { glyphMarginClassName }
+          }
+        ]
 
-        console.log(
-          JSON.parse(JSON.stringify(decorationMap[position.lineNumber].config)).options.glyphMarginClassName
-        )
-
-        decorationMap[position.lineNumber].instance.set([decorationMap[position.lineNumber].config])
+        // 更新断点 UI
+        editor.getModel().deltaDecorations([currLineDot.id], newDecorations)
       }
     })
 
     editor.onMouseLeave(() => {
-      // 清除未设置的断点 UI
+      const allBreakDots = getAllBreakDots()
 
-      Object.keys(decorationMap).forEach(lineNumber => {
-        if (
-          !decorationMap[lineNumber].config.options.glyphMarginClassName.includes('myGlyphMarginClass-active')
-        ) {
-          const curLineDecoration = decorationMap[lineNumber].instance
+      const unActives = allBreakDots.filter(
+        item => !item.options.glyphMarginClassName.includes('myGlyphMarginClass-active')
+      )
 
-          curLineDecoration.clear()
-          Reflect.deleteProperty(decorationMap, lineNumber)
-        }
-      })
+      editor.getModel().deltaDecorations(
+        unActives.map(item => item.id),
+        []
+      )
     })
 
     editor.onContextMenu(function (e) {
       console.log('contextmenu - ', e)
     })
-    // editor.onMouseLeave(function (e) {
-    //   console.log('mouseleave', e)
-    // })
+
+    function mouseEnterLine(lineNumber: number) {
+      // console.log('移入了', lineNumber)
+
+      const allBreakDots = getAllBreakDots()
+      if (allBreakDots.some(item => item.range.startLineNumber === lineNumber)) {
+        return
+      }
+
+      const config = {
+        range: new monaco.Range(lineNumber, 1, lineNumber, 1),
+        options: { glyphMarginClassName: 'myGlyphMarginClass' }
+      }
+
+      editor.createDecorationsCollection([config])
+    }
+
+    function mouseLeaveLine(lineNumber: number) {
+      // console.log('移出了', currLineNumber)
+
+      const allBreakDots = getAllBreakDots()
+
+      const currLineDot = allBreakDots.find(item => item.range.startLineNumber === lineNumber)
+
+      if (!currLineDot) {
+        return
+      }
+
+      if (currLineDot.options.glyphMarginClassName.includes('myGlyphMarginClass-active')) {
+        return
+      }
+
+      editor.getModel().deltaDecorations([currLineDot.id], [])
+    }
   }, [])
 
   const ref = useRef<monaco.editor.IEditorDecorationsCollection>()
@@ -177,10 +183,26 @@ function App() {
     ])
   }
 
+  const getModel = () => {
+    const model = editor.getModel()
+
+    model.deltaDecorations
+
+    const des = model.getAllDecorations()
+
+    const breakDots = des.filter(item => item.options.glyphMarginClassName?.includes('myGlyphMarginClass'))
+
+    console.log(breakDots)
+  }
+
   return (
     <>
       <button onClick={setBreak} onMouseDown={evt => evt.preventDefault()}>
         set
+      </button>
+
+      <button onClick={getModel} onMouseDown={evt => evt.preventDefault()}>
+        model
       </button>
 
       <div id="container" style={{ width: 1000, height: 800 }}></div>
@@ -189,130 +211,3 @@ function App() {
 }
 
 export default App
-
-const jsCode = `
-const rect_A = document.querySelector('.rect_A')
-const rect_B = document.querySelector('.rect_B')
-
-const overlapRect = document.querySelector('.overlap')
-
-let rect_A_pos
-let rect_B_pos
-addDrag(rect_A, (left, top, rect) => {
-  rect_A_pos = {
-    leftTop: { x: left, y: top },
-    rightBottom: { x: left + rect.width, y: top + rect.height }
-  }
-
-  drawOverlay()
-})
-
-addDrag(rect_B, (left, top, rect) => {
-  rect_B_pos = {
-    leftTop: { x: left, y: top },
-    rightBottom: { x: left + rect.width, y: top + rect.height }
-  }
-
-  drawOverlay()
-})
-
-function addDrag(el, Move) {
-  function handleMove(evt) {
-    evt.preventDefault()
-    const { clientX, clientY } = evt
-
-    const left = clientX - an.left
-    const top = clientY - an.top
-
-    el.style.left = left + 'px'
-    el.style.top = top + 'px'
-
-    Move(left, top, el?.getBoundingClientRect())
-  }
-
-  let an = { left: 0, top: 0 }
-  el.addEventListener('mousedown', evt => {
-    const { clientX, clientY } = evt
-
-    const rect = el?.getBoundingClientRect()
-    an = { left: clientX - rect.left, top: clientY - rect.top }
-
-    document.addEventListener('mousemove', handleMove)
-  })
-
-  document.addEventListener('mouseup', () => {
-    document.removeEventListener('mousemove', handleMove)
-  })
-}
-
-function drawOverlay() {
-  if (!rect_A_pos || !rect_B_pos) {
-    return
-  }
-
-  const leftTop = {
-    x: Math.max(rect_A_pos.leftTop.x, rect_B_pos.leftTop.x),
-    y: Math.max(rect_A_pos.leftTop.y, rect_B_pos.leftTop.y)
-  }
-
-  const rightBottom = {
-    x: Math.min(rect_A_pos.rightBottom.x, rect_B_pos.rightBottom.x),
-    y: Math.min(rect_A_pos.rightBottom.y, rect_B_pos.rightBottom.y)
-  }
-
-  let overlapWidth = rightBottom.x - leftTop.x
-  let overlayHeight = rightBottom.y - leftTop.y
-
-  if (overlapWidth < 0 || overlayHeight < 0) {
-    overlapRect.style.display = 'none'
-    return
-  }
-
-  overlapRect.style.display = 'initial'
-
-  overlapRect.style.left = leftTop.x + 'px'
-  overlapRect.style.top = leftTop.y + 'px'
-  overlapRect.style.width = rightBottom.x - leftTop.x + 'px'
-  overlapRect.style.height = rightBottom.y - leftTop.y + 'px'
-}
-`
-
-/**
- * 
- * 
-      // 点击行号时
-      if (evt.target.type === monaco.editor.MouseTargetType.GUTTER_LINE_NUMBERS) {
-        return
-
-        const { position } = evt.target
-        const decorations = editor.createDecorationsCollection([
-          {
-            range: new monaco.Range(position.lineNumber, 1, position.lineNumber, 1),
-            options: {
-              // isWholeLine: true,
-              // className: 'myContentClass',
-              glyphMarginClassName: 'myGlyphMarginClass',
-              glyphMarginHoverMessage: { value: '断点' }
-            }
-          }
-        ])
-        console.log(decorations)
-
-        setTimeout(() => {
-          decorations.set([
-            {
-              range: new monaco.Range(position.lineNumber, 1, position.lineNumber, 1),
-              options: {
-                isWholeLine: true,
-                className: 'myContentClass',
-                glyphMarginClassName: 'myGlyphMarginClass-2',
-                glyphMarginHoverMessage: { value: '断点' }
-              }
-            }
-          ])
-          // decorations.clear()
-        }, 2000)
-      }
-
-      
- */
---
+++ src/code.ts
@@ -0,0 +1,126 @@
+export const jsCode = `
+const rect_A = document.querySelector('.rect_A')
+const rect_B = document.querySelector('.rect_B')
+
+const overlapRect = document.querySelector('.overlap')
+
+let rect_A_pos
+let rect_B_pos
+addDrag(rect_A, (left, top, rect) => {
+  rect_A_pos = {
+    leftTop: { x: left, y: top },
+    rightBottom: { x: left + rect.width, y: top + rect.height }
+  }
+
+  drawOverlay()
+})
+
+addDrag(rect_B, (left, top, rect) => {
+  rect_B_pos = {
+    leftTop: { x: left, y: top },
+    rightBottom: { x: left + rect.width, y: top + rect.height }
+  }
+
+  drawOverlay()
+})
+
+function addDrag(el, Move) {
+  function handleMove(evt) {
+    evt.preventDefault()
+    const { clientX, clientY } = evt
+
+    const left = clientX - an.left
+    const top = clientY - an.top
+
+    el.style.left = left + 'px'
+    el.style.top = top + 'px'
+
+    Move(left, top, el?.getBoundingClientRect())
+  }
+
+  let an = { left: 0, top: 0 }
+  el.addEventListener('mousedown', evt => {
+    const { clientX, clientY } = evt
+
+    const rect = el?.getBoundingClientRect()
+    an = { left: clientX - rect.left, top: clientY - rect.top }
+
+    document.addEventListener('mousemove', handleMove)
+  })
+
+  document.addEventListener('mouseup', () => {
+    document.removeEventListener('mousemove', handleMove)
+  })
+}
+
+function drawOverlay() {
+  if (!rect_A_pos || !rect_B_pos) {
+    return
+  }
+
+  const leftTop = {
+    x: Math.max(rect_A_pos.leftTop.x, rect_B_pos.leftTop.x),
+    y: Math.max(rect_A_pos.leftTop.y, rect_B_pos.leftTop.y)
+  }
+
+  const rightBottom = {
+    x: Math.min(rect_A_pos.rightBottom.x, rect_B_pos.rightBottom.x),
+    y: Math.min(rect_A_pos.rightBottom.y, rect_B_pos.rightBottom.y)
+  }
+
+  let overlapWidth = rightBottom.x - leftTop.x
+  let overlayHeight = rightBottom.y - leftTop.y
+
+  if (overlapWidth < 0 || overlayHeight < 0) {
+    overlapRect.style.display = 'none'
+    return
+  }
+
+  overlapRect.style.display = 'initial'
+
+  overlapRect.style.left = leftTop.x + 'px'
+  overlapRect.style.top = leftTop.y + 'px'
+  overlapRect.style.width = rightBottom.x - leftTop.x + 'px'
+  overlapRect.style.height = rightBottom.y - leftTop.y + 'px'
+}
+`
+
+/**
+ * 
+ * 
+      // 点击行号时
+      if (evt.target.type === monaco.editor.MouseTargetType.GUTTER_LINE_NUMBERS) {
+        return
+
+        const { position } = evt.target
+        const decorations = editor.createDecorationsCollection([
+          {
+            range: new monaco.Range(position.lineNumber, 1, position.lineNumber, 1),
+            options: {
+              // isWholeLine: true,
+              // className: 'myContentClass',
+              glyphMarginClassName: 'myGlyphMarginClass',
+              glyphMarginHoverMessage: { value: '断点' }
+            }
+          }
+        ])
+        console.log(decorations)
+
+        setTimeout(() => {
+          decorations.set([
+            {
+              range: new monaco.Range(position.lineNumber, 1, position.lineNumber, 1),
+              options: {
+                isWholeLine: true,
+                className: 'myContentClass',
+                glyphMarginClassName: 'myGlyphMarginClass-2',
+                glyphMarginHoverMessage: { value: '断点' }
+              }
+            }
+          ])
+          // decorations.clear()
+        }, 2000)
+      }
+
+      
+ */
